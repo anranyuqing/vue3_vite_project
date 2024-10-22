7fd779080a09db65fa1a07b2d74035a3857e259b
lixw <GyTemp13579>
2024-10-22 18:44:58 +0800

    测试

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,14 @@
-
 <template>
-<div>22</div>
+  <button @click="initData">发送请求</button>
 </template>
 
 <script setup lang="ts">
-import {useAuthStore} from '@/store'
+import { useAuthStore } from '@/store'
+import { getPermissionInfo } from '@/server/api'
 
+async function initData() {
+  const data = await getPermissionInfo({})
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
